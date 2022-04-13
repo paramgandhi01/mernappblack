73f8e7a4f7c5e2dd2de1721acdbfa7ee0440c1f3
paramgandhi01 <92042054+paramgandhi01@users.noreply.github.com>
2022-04-13 13:02:48 +0530

    Add files via upload

---
+++ modules/blogPost.js
@@ -0,0 +1,15 @@
+const mongoose=require("mongoose")
+//schema
+const Schema=mongoose.Schema;
+const BlogPostSchema=new Schema({
+    title:String,
+    body:String,
+    date:{
+        type: String,
+        default:Date.now()
+    }
+});
+//model
+const BlogPost=mongoose.model("BlogPost",BlogPostSchema)
+
+module.exports=BlogPost;
